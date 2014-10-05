bbadd07be647c289fc5d2585c6f21b07f4a29f46
Andrew Sorensen <andrew@andrewsorensen.net>
2014-10-05 13:34:12 -0700

    Add sensor basics

--- sensors/admin.py
+++ sensors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from sensors.models import SensorType, Sensor
 
-# Register your models here.
+class SensorTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    pass
+
+admin.site.register(SensorType, SensorTypeAdmin)
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    pass
+
+admin.site.register(Sensor, SensorAdmin)
--- sensors/models.py
+++ sensors/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class SensorType(models.Model):
+    name = models.CharField(max_length=128)
+    
+class Sensor(models.Model):
+    name = models.CharField(max_length=128)
+    location = models.ForeignKey('portal.Residence')
