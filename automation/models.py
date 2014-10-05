dc266ea28c8ffd5ef0f6eef46eb4674882421a1c
Andrew Sorensen <andrew@andrewsorensen.net>
2014-10-05 15:12:57 -0700

    Make Lights relate to a Room

--- automation/models.py
+++ automation/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.core.validators import URLValidator
+from portal.models import Room
 
 class Light(models.Model):
     name = models.CharField(max_length=128)
-    location = models.CharField(max_length=128)
+    location = models.ForeignKey('portal.Room')
     description = models.TextField(blank=True)
-    
+
+    def turn_on(self):
+        pass
+
+    def turn_off(self):
+        pass
+
     def __unicode__(self):
         return self.name
     
@@ -16,9 +23,10 @@ class Mood(models.Model):
     title = models.CharField(max_length=128)
     enabled = models.BooleanField(default=False)
     description = models.TextField(blank=True)
+
     def __unicode__(self):
         return title
-    
+
     def __str__(self):
         return self.title
     
