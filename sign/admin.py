1ae0089a0636b8ffa348df5f56fff1869be15775
Andrew Sorensen <andrew@andrewsorensen.net>
2014-10-05 15:58:16 -0700

    Add sign widgets

--- sign/admin.py
+++ sign/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from sign.models import Sign
+from sign.models import Sign, Widget
 
 class SignAdmin(admin.ModelAdmin):
     list_display = ('name', 'location')
+    save_as = True
     pass
 
 admin.site.register(Sign, SignAdmin)
+
+class WidgetAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    save_as = True
+    pass
+
+admin.site.register(Widget, WidgetAdmin)
--- sign/models.py
+++ sign/models.py
@@ -4,5 +4,26 @@ from portal.models import Residence
 class Sign(models.Model):
     name = models.CharField(max_length=128)
     hostname = models.CharField(max_length=128)
-    location = models.ForeignKey('portal.Residence')
+    location = models.ForeignKey('portal.Room')
     is_available = models.BooleanField(default=True)
+    widgets = models.ManyToManyField('Widget', blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name + " (" + self.location.name + ")"
+
+    def __str__(self):
+        return self.name + " (" + self.location.name + ")"
+
+class Widget(models.Model):
+    name = models.CharField(max_length=128)
+    internal_name = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+class SignWidgetConfig(models.Model):
+    sign = models.ForeignKey('Sign')
+    widget = models.ForeignKey('Widget')
