a1fb603066122e53575a0e1899626c39706e767e
Andrew Sorensen <andrew@andrewsorensen.net>
2014-10-05 23:34:45 -0700

    Only apply context processor to authenticated users

--- portal/context_processors/commons.py
+++ portal/context_processors/commons.py
@@ -1,5 +1,8 @@
 from portal.models import Message, Residence
 
 def add_commons(request):
-    return {'portal_messages': Message.objects.filter(user=request.user, acknowledged=False),
-            'portal_residences': Residence.objects.filter(tenants=request.user)}
+    d = {}
+    if request.user.is_authenticated():
+        d =  {'portal_messages': Message.objects.filter(user=request.user, acknowledged=False),
+                'portal_residences': Residence.objects.filter(tenants=request.user)}
+    return d
