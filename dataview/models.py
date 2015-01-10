114139184139ff3d358a55d559758144535bdd08
Andrew Sorensen <andrew@andrewsorensen.net>
2015-01-10 15:41:22 -0800

    Add documentation

--- dataview/models.py
+++ dataview/models.py
@@ -2,9 +2,18 @@ from django.db import models
 from django.contrib.gis.db import models
 from portal.models import Address
 
+
 class SystemDeployment(models.Model):
+    """ System Deployments represent a server that hosts Dataview."""
+
+    """ the name of the dataview deployment. Whatever you want"""
     name = models.CharField(max_length=128)
+
+    """ the FQDN that can be used to manage this specific dataview deployment"""
     hostname = models.CharField(max_length=128)
+
+    """ the physical location of the dataview server"""
     location = models.ForeignKey('portal.Address')
+
     is_master = models.BooleanField(default=True)
     is_available = models.BooleanField(default=True)
