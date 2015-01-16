152933e39489488024b7a5f9679dbe421e9bc4c1
Andrew Sorensen <andrew@andrewsorensen.net>
2015-01-15 22:06:24 -0800

    Add app-specific url router

--- dataview/middleware/router.py
+++ dataview/middleware/router.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+import re
 
 class AppSpecificURLConfLoader():
 
   def process_request(self, request):
-    request.urlconf = ''
+    match = re.match('^/([a-zA-Z0-9]*).*', request.path)
+    if match.group(1) in settings.INSTALLED_APPS:
+      request.urlconf = match.group(1) + '.urls'
     return None
