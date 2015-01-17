1c01274d79b6453940ff3c1bbce3b95ba1a65ab1
Andrew Sorensen <andrew@andrewsorensen.net>
2015-01-17 13:37:12 -0800

    Remove application-specific urls.py includes

--- dataview/urls.py
+++ dataview/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,13 +9,12 @@ admin.site.site_title = "WLNet DataView"
 
 urlpatterns = patterns('',
     url(r'^$', 'portal.views.index', name='index'),
+    url(r'^api/1/config$', 'dataview.views.v1_config', name='v1_config'),
     url(r'^api/', include('api.urls')),
-    
-    # app specific includes
-    url(r'^portal/', include('portal.urls')),
-    url(r'^sign/', include('sign.urls')),
-    url(r'^transportation/', include('transportation.urls')),
 
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^account/login/$',  login),
+    (r'^account/logout/$', logout),
 )
