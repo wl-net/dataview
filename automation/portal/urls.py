768541e69b333a77ea826f002449aebb51390259
Andrew Sorensen <andrew@andrewsorensen.net>
2015-01-18 18:41:42 -0800

    Cleanup urlconf for automation

--- automation/portal/urls.py
+++ automation/portal/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('automation.views',
-    url(r'(?P<residence>[0-9]+)/speakers$', 'control_speakers', name='control_speakers'),
-    url(r'(?P<residence>[0-9]+)/add-speaker$', 'add_speaker', name='add_speaker'),
-    url(r'(?P<residence>[0-9]+)/edit-speaker/(?P<speaker>[0-9]+)$', 'edit_speaker', name='edit_speaker'),
-    
-    url(r'^automation/(?P<residence>[0-9]+)/speakers$', 'control_speakers', name='control_speakers'),
+    url(r'^/?$', 'index', name = 'automation-index'),
+    url(r'^/(?P<residence>[0-9]+)/speakers$', 'speakers', name='automation-speakers'),
+    url(r'^/(?P<residence>[0-9]+)/add-speaker$', 'add_speaker', name='add_speaker'),
+    url(r'^/(?P<residence>[0-9]+)/edit-speaker/(?P<speaker>[0-9]+)$', 'edit_speaker', name='edit_speaker'),
 )
--- automation/views.py
+++ automation/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponse
 from portal.models import Residence, Room
 from automation.models import Speaker, SpeakerForm
 
-def control_speakers(request, residence):
+def index(request):
+    return render_to_response('automation/index.html', RequestContext(request, {}))
+
+def speakers(request, residence):
     speakers = Speaker.objects.filter(location = Room.objects.filter(location = Residence.objects.get(id = residence, tenants = request.user))).order_by('location')
 
     return render_to_response('automation/speakers.html', RequestContext(request, {'speakers': speakers}))
