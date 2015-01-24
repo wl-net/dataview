37cd2c69b8c6134ef4cec6fb20f0980009256fee
Andrew Sorensen <andrew@andrewsorensen.net>
2015-01-24 14:27:11 -0800

    Add abstract decider

---
+++ automation/deciders/abstract.py
@@ -0,0 +1,20 @@
+class AbstractDecider:
+    def __init__():
+        pass
+
+    def decide(self):
+        """
+        Determines the outcome of the decider.
+        """
+        return False
+
+    def fuzzy_decide():
+        """
+        Returns a numeric value representing the outcome of the decider (integers between 0-1)
+        """
+        return 0
+
+    def get_decision_reason(self):
+        """
+        Returns a OrderedDict of decisions and their results.
+        """
