cd82c5ea1a3fe4b41572f1968d0573a7b5eb2771
Andrew Sorensen <andrew@andrewsorensen.net>
2015-01-25 19:12:43 -0800

    Add get_instance()

--- automation/models.py
+++ automation/models.py
@@ -2,16 +2,27 @@ from django.db import models
 from django.forms import ModelForm
 from django.core.validators import URLValidator
 from portal.models import Room
+from importlib import import_module
+import sys, json
 
 class Automator(models.Model):
-    host = models.GenericIPAddressField()
-    authorization = models.TextField()
+    name = models.CharField(max_length=60)
+    account = models.ForeignKey('dataview.Account')
+    cls = models.CharField(max_length=128)
+    description = models.TextField()
+    configuration = models.TextField()
 
     def __unicode__(self):
-        return self.host
+        return self.name
 
     def __str__(self):
-        return self.host
+        return self.name
+
+    def get_instance(self):
+        import_module(self.cls[:self.cls.rfind(".")])
+        module = sys.modules[self.cls[:self.cls.rfind(".")]]
+        clsName = getattr(module, self.cls[(self.cls.rfind(".")+1):len(self.cls)])
+        return clsName(json.loads(self.configuration))
 
 class Decider(models.Model):
     name = models.CharField(max_length=128)
@@ -27,14 +38,13 @@ class Controller(models.Model):
     description = models.TextField()
     account = models.ForeignKey('dataview.Account')
     deciders = models.ManyToManyField('automation.Decider', through='ControllerDecider')
-    automator = models.ForeignKey('dataview.Automator')
+    automator = models.ForeignKey('automation.Automator')
 
     def __unicode__(self):
         return self.name
 
     def __str__(self):
         return self.name
-
 
 class ControllerForm(ModelForm):
     class Meta:
