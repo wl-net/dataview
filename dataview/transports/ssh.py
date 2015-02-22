840b423e79b229021f0dac826ee75dc4026e62b1
Andrew Sorensen <andrew@andrewsorensen.net>
2015-02-21 21:56:00 -0800

    Add ssh transport

--- dataview/transports/ssh.py
+++ dataview/transports/ssh.py
@@ -2,27 +2,30 @@ import paramiko
 
 class SSH:
     def __init__(self):
-        client = paramiko.SSHClient()
+        self.client = paramiko.SSHClient()
 
     def get_ssh_key(self, file=None):
         key = ""
-        if file = None:
+        if file is None:
             key = ""
         else: 
             key = ""
         return paramiko.RSAKey(data=base64.decodestring(key))
     
-    def _connect(self, hostname, username, ssh_key):
+    def _connect(self, username, hostname, port, ssh_key):
+        self.client.connect(hostname, port, username)
         pass
 
-    def connect(self, hostname):
+    def connect(self, username, hostname, port=22):
+
+        self._connect(username, hostname, port, key)
         pass
 
     def disconnect(self):
-        client.close()
+        self.client.close()
 
     def exec_command(self, command):
-        return client.exec_command(command)
+        return self.client.exec_command(command)
 
     def get_client(self):
-        return client
+        return self.client
