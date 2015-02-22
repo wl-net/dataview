1aa5170eecfea3eee319d065363ab2570354d07e
Andrew Sorensen <andrew@andrewsorensen.net>
2015-02-22 12:12:01 -0800

    Add Transportation widget

---
+++ transportation/sign_widgets/transportation/__init__.py
@@ -0,0 +1,11 @@
+from sign.sign_widgets import AbstractWidget
+
+class TransportationWidget(AbstractWidget):
+
+    WIDGET_NAME = "Transportation"
+
+    def __init__(self):
+        pass
+
+    def get_contents(self):
+        pass
