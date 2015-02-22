c6d17eb851a93f883dd68238ce912aae8e41fde8
Andrew Sorensen <andrew@andrewsorensen.net>
2015-02-21 23:12:41 -0800

    Add dashing dashboard

--- sign/views.py
+++ sign/views.py
@@ -34,3 +34,63 @@ def sign(request, id=None):
     template_fields['x_sign_info'] = json.dumps({'signId': sign.id})
 
     return render_to_response('sign/sign.html', RequestContext(request, template_fields))
+
+def sign_dashing(request, id=None):
+    return render_to_response('sign/dashing-sign.html', RequestContext(request, {}))
+
+def sign_config(request, id=None):
+  response = """/* global $, Dashboard */
+
+    var dashboard = new Dashboard();
+
+    dashboard.addWidget('clock_widget', 'Clock');
+
+    dashboard.addWidget('humidity', 'Number', {
+        getData: function () {
+            $.extend(this.data, {
+                title: 'Current Valuation',
+                more_info: 'In billions',
+                updated_at: 'Last updated at 14:10',
+                detail: '64%',
+                value: '$35'
+            });
+        }
+    });
+
+    dashboard.addWidget('buzzwords_widget', 'List', {
+        getData: function () {
+            $.extend(this.data, {
+                title: 'Travel Times',
+                more_info: '# of times said around the office',
+                updated_at: 'Last updated at 18:58',
+                data: [{label: 'Exit strategy', value: 24},
+                      {label: 'Web 2.0', value: 12},
+                      {label: 'Turn-key', value: 2},
+                      {label: 'Enterprise', value: 12},
+                      {label: 'Pivoting', value: 3},
+                      {label: 'Leverage', value: 10},
+                      {label: 'Streamlininess', value: 4},
+                      {label: 'Paradigm shift', value: 6},
+                      {label: 'Synergy', value: 7}]
+            });
+        }
+    });
+
+    dashboard.addWidget('convergence_widget', 'Graph', {
+        getData: function () {
+            $.extend(this.data, {
+                title: 'Convergence',
+                value: '41',
+                more_info: '',
+                data: [ 
+                        { x: 0, y: 40 }, 
+                        { x: 1, y: 49 }, 
+                        { x: 2, y: 38 }, 
+                        { x: 3, y: 30 }, 
+                        { x: 4, y: 32 }
+                    ]
+                });
+        }
+    });""";
+
+  return HttpResponse(response, content_type="text/javascript")
