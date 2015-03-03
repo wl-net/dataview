f29e744a9b8f99179310c07f06fc946e7f510fd4
Andrew Sorensen <andrew@andrewsorensen.net>
2015-03-03 15:46:28 -0800

    Cleanup

--- portal/views.py
+++ portal/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import json
 from portal.models import *
-from security.models import Camera
 
-# homepage
 def index(request):
     return render_to_response('portal/index.html', RequestContext(request, {}))
