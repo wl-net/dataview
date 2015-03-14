c858647685069240df254f824c4ddb457a81f85f
Andrew Sorensen <andrew@andrewsorensen.net>
2015-03-14 16:32:38 -0700

    Complete sensor data models

--- sensors/models.py
+++ sensors/models.py
@@ -1,13 +1,33 @@
 from django.db import models
+import datetime
 
 class SensorType(models.Model):
     name = models.CharField(max_length=128)
-    
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
 class Sensor(models.Model):
     name = models.CharField(max_length=128)
-    location = models.ForeignKey('portal.Residence')
+    type = models.ForeignKey(SensorType)
+    description = models.TextField(blank=True)
+    location = models.ForeignKey('portal.Room')
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
 
 class SensorValue(models.Model):
     sensor = models.ForeignKey('sensors.Sensor')
-    updated = models.DateTimeField()
+    updated = models.DateTimeField(default=datetime.datetime.now())
     value = models.TextField()
+
+    def __unicode__(self):
+        return self.sensor + " " + self.updated
+
+    def __str__(self):
+        return self.sensor + " " + self.updated
