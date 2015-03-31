408c97945a47e84bb35cc2880a95e8fad5aff919
Andrew Sorensen <andrew@andrewsorensen.net>
2015-03-30 22:35:06 -0700

    Add sign sensor value widget

--- sensors/sign_widgets/sensor_value/__init__.py
+++ sensors/sign_widgets/sensor_value/__init__.py
@@ -1,8 +1,18 @@
 from sign.sign_widgets import AbstractWidget
+from sensors.models import SensorValue
 
 class SensorValueWidget(AbstractWidget):
 
     WIDGET_NAME = "Generic Sensor Value Widget"
 
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        pass
+
     def get_contents(self):
-        return ""
+        try:
+            return SensorValue.objects.filter(sensor=self.configuration["sensor"]).order_by('updated')[0].value
+        except Exception:
+            return "Unknown"
+
+      
