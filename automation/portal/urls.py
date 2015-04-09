a4ef85971f9e0ddaa4145578a4fa13d6f2131619
Andrew Sorensen <andrew@andrewsorensen.net>
2015-04-08 22:39:27 -0700

    Make automation application work again

--- automation/portal/urls.py
+++ automation/portal/urls.py
@@ -7,19 +7,15 @@ urlpatterns = patterns('automation.views',
 
     url(r'^/automate$', AutomateWizard.as_view([AutomatorForm, DeciderForm, ControllerForm], template_name='automation/automate-wizard.html')),
 
-    url(r'^/(?P<residence>[0-9]+)/speakers$', 'speakers', name='automation-speakers'),
-    url(r'^/(?P<residence>[0-9]+)/add-speaker$', 'add_speaker', name='add_speaker'),
-    url(r'^/(?P<residence>[0-9]+)/edit-speaker/(?P<speaker>[0-9]+)$', 'edit_speaker', name='edit_speaker'),
-
     url(r'^/automators/$', 'automators', name='automation-automators'),
     url('^/add-automator$', 'add_automator', name='automation-add_automator'),
-    url('^/edit-automator/(?P<automator>[0-9]+)$', 'edit_automator', name='automation-edit_automator'),
+    url('^/edit-automator/(?P<automator>[0-9a-f\-]+)$', 'edit_automator', name='automation-edit_automator'),
 
     url(r'^/controllers/$', 'controllers', name='automation-controllers'),
     url('^/add-controller$', 'add_controller', name='automation-add_controller'),
-    url('^/edit-controller/(?P<controller>[0-9]+)$', 'edit_controller', name='automation-edit_controller'),
+    url('^/edit-controller/(?P<controller>[0-9a-f\-]+)$', 'edit_controller', name='automation-edit_controller'),
 
     url(r'^/deciders/$', 'deciders', name='automation-deciders'),
     url('^/add-decider', 'add_decider', name='automation-add_decider'),
-    url('^/edit-decider/(?P<decider>[0-9]+)$', 'edit_decider', name='automation-edit_decider'),
+    url('^/edit-decider/(?P<decider>[0-9a-f\-]+)$', 'edit_decider', name='automation-edit_decider'),
 )
--- automation/views.py
+++ automation/views.py
@@ -2,9 +2,9 @@ from django.template import RequestContext
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
+from portal.models import Event
 from dataview.models import Account
-from portal.models import Residence, Room, Event
-from automation.models import Automator, Controller, Decider, AutomatorForm, ControllerForm, DeciderForm, Speaker, SpeakerForm
+from automation.models import Automator, Controller, Decider, AutomatorForm, ControllerForm, DeciderForm
 
 def index(request):
     automators = Automator.objects.all()
@@ -20,20 +20,6 @@ class AutomateWizard(SessionWizardView):
     def done(self, form_list, **kwargs):
         do_something_with_the_form_data(form_list)
         return HttpResponseRedirect('/portal/automation/')
-
-def speakers(request, residence):
-    speakers = Speaker.objects.filter(location = Room.objects.filter(location = Residence.objects.get(id = residence, tenants = request.user))).order_by('location')
-
-    return render_to_response('automation/speakers.html', RequestContext(request, {'speakers': speakers}))
-
-def add_speaker(request, residence):
-    form = SpeakerForm()
-    return render_to_response('automation/add-speaker.html', RequestContext(request, {'form': form}))
-
-def edit_speaker(request, residence, speaker):
-    form = SpeakerForm(instance = Speaker.objects.get(id=speaker))
-
-    return render_to_response('automation/edit-speaker.html', RequestContext(request, {'form': form}))
 
 def automators(request):
     automators = Automator.objects.all()
