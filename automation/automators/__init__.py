d59e8c91bd111d54ffb03450154afe3713257a77
Andrew Sorensen <andrew@andrewsorensen.net>
2015-04-09 08:16:44 -0700

    Add stubs to make configuration easier

--- automation/automators/__init__.py
+++ automation/automators/__init__.py
@@ -2,3 +2,9 @@ class AbstractAutomator:
 
     def __init__(self, configuration):
         pass
+
+    def get_configuration_parameters():
+        pass
+
+    def validate_configuration(self):
+        pass
