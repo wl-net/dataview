e299d08e7728f5bdfd49f7ae16f9d39e89206520
Andrew Sorensen <andrew@andrewsorensen.net>
2015-04-08 21:57:13 -0700

    Fix sign url routing for uuid based models

--- sign/urls.py
+++ sign/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from sign.api import urls
 
 urlpatterns = patterns('',
-    url(r'^sign/(?P<id>[0-9]+)$', 'sign.views.sign', name='sign'),
-    url(r'^sign/(?P<id>[0-9]+)/dashing$', 'sign.views.sign_dashing', name='sign_dashing'),
-    url(r'^sign/(?P<id>[0-9]+)/config$', 'sign.views.sign_config', name='sign_config'),
-    url(r'^sign/(?P<id>[0-9]+)/widget/(?P<widget_id>[0-9]+)$', 'sign.views.sign_widget', name='sign_widget'),
+    url(r'^sign/(?P<id>[0-9a-f\-]+)$', 'sign.views.sign', name='sign'),
+    url(r'^sign/(?P<id>[0-9a-f\-]+)/dashing$', 'sign.views.sign_dashing', name='sign_dashing'),
+    url(r'^sign/(?P<id>[0-9a-f\-]+)/config$', 'sign.views.sign_config', name='sign_config'),
+    url(r'^sign/(?P<id>[0-9a-f\-]+)/widget/(?P<widget_id>[0-9a-f\-]+)$', 'sign.views.sign_widget', name='sign_widget'),
 )
