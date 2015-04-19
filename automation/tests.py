6d673ebca616c51f1448057a4675fbb42e199dc3
Andrew Sorensen <andrew@andrewsorensen.net>
2015-04-18 22:20:00 -0700

    Remove speaker tests

--- automation/tests.py
+++ automation/tests.py
@@ -1,18 +1,5 @@
 from django.test import TestCase
 
-from automation.models import Speaker
-
-class SpeakerTest(TestCase):
-    def test_mute(self):
-        s = Speaker()
-        s.set_volume(25)
-
-        s.mute()
-        self.assertEqual(s.get_volume(), 0)
-
-        s.unmute()
-        self.assertEqual(s.get_volume(), 25)
-
 from automation.deciders.time import TimeDecider
 import datetime
 
