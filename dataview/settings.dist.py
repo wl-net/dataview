8a492a6e5c61945a74ceacea12545662c8c192ac
Andrew Sorensen <andrew@andrewsorensen.net>
2015-04-18 22:01:12 -0700

    change default role

--- dataview/settings.dist.py
+++ dataview/settings.dist.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dataview',                      # Or path to database file if using sqlite3.
+        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
