366cf353bc02ad367442c570be361242ead676f7
Andrew Sorensen <andrew@andrewsorensen.net>
2015-05-16 14:53:17 -0700

    Add air cleaner automator

---
+++ automation/automators/air_cleaner/jsonrpc.py
@@ -0,0 +1,15 @@
+from dataview.transports.json_rpc import JSONRPCClient
+from automation.automators import AbstractAutomator
+
+class AirCleanerJSONRPCAutomator(AbstractAutomator):
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        self.client = JSONRPCClient()
+        self.client.connect(configuration['target'], configuration['token'], configuration['certificate'])
+
+    def set_fan_speed(self, fan_speed):
+        self.client.call('call_function', ['acfanspeed', [fan_speed.upper()]])
+
+
+    def get_commands(self):
+        return {"set_fan_speed":{"args":[["off", "low", "medium", "high"]]}}
