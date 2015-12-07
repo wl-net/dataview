2da927da2d6739342136543eabfdf91448b63fd5
Andrew Sorensen <andrew@andrewsorensen.net>
2015-12-06 20:14:00 -0800

    Cleanup for Django 1.9+

--- dataview/urls.py
+++ dataview/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib.auth.views import login, logout
 
 from django.contrib import admin
@@ -10,7 +10,7 @@ admin.site.site_title = "WLNet DataView"
 from portal.views import index
 from dataview.views import v1_config
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^api/1/config$', v1_config, name='v1_config'),
     url(r'^api/', include('api.urls')),
@@ -18,6 +18,6 @@ urlpatterns = patterns('',
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     
-    (r'^account/login/$',  login),
-    (r'^account/logout/$', logout),
-)
+    url(r'^account/login/$',  login),
+    url(r'^account/logout/$', logout)
+]
