7a5fb4146ea744a7be1ec2bc8dffa1bfec4e3dde
Andrew Sorensen <andrew@andrewsorensen.net>
2016-01-02 15:54:28 -0800

    Add RooWifi automator

---
+++ automation/automators/roowifi/jsonrpc.py
@@ -0,0 +1,14 @@
+from dataview.transports.json_rpc import JSONRPCClient
+from automation.automators import AbstractAutomator
+
+class RoombaRooWifiAutomator(AbstractAutomator):
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        self.client = JSONRPCClient()
+        self.client.connect(configuration['target'], configuration['token'], configuration['certificate'])
+
+    def clean(self):
+        self.client.call('clean', [])
+
+    def dock(self):
+        self.client.call('dock', [])
