062dbf6f5e65fddbecff7fb48aaacf0b12062f54
Andrew Sorensen <andrew@andrewsorensen.net>
2016-01-02 16:33:49 -0800

    Style cleanup

--- transportation/provider/opentripplanner.py
+++ transportation/provider/opentripplanner.py
@@ -1,18 +1,16 @@
-import requests, json, datetime
+import requests, datetime
 
 class OpenTripPlannerProvider():
   
-  def __init__(self, api_target, router_id):
-    self.api_target = api_target
-    self.router_id = router_id
-    
-  def get_directions(self, fromPlace, toPlace, time='', date=datetime.date.today().strftime('%m-%d-%Y'), mode='WALK,TRANSIT', maxWalkDistance = 1000, arriveBy=False, wheelchair=False, showIntermediateStops=False):
-    print(datetime.date.today().strftime('%m-%d-%Y'))
-    r = requests.get(self.api_target + 'otp/routers/' + self.router_id + '/plan', params = 
+    def __init__(self, api_target, router_id):
+        self.api_target = api_target
+        self.router_id = router_id
+
+    def get_directions(self, fromPlace, toPlace, time='', date=datetime.date.today().strftime('%m-%d-%Y'), mode='WALK,TRANSIT', maxWalkDistance = 1000, arriveBy=False, wheelchair=False, showIntermediateStops=False):
+        r = requests.get(self.api_target + 'otp/routers/' + self.router_id + '/plan', params =
                      {'fromPlace': fromPlace, 'toPlace': toPlace, 'time': time, 'date': date, 'mode': mode, 'maxWalkDistance': maxWalkDistance,
                       'arriveBy': arriveBy, 'wheelchair': wheelchair, 'showIntermediateStops': showIntermediateStops}
                      , headers = {'Accept': 'application/json'})
-    print(r.url)
-    if r.status_code != 200:
-      pass
-    return r.json()['plan']
+        if r.status_code != 200:
+            pass
+        return r.json()['plan']
