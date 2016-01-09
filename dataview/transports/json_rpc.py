9bc43c038ad638e38a436f48005da260862a0881
Andrew Sorensen <andrew@andrewsorensen.net>
2016-01-09 09:59:02 -0800

    Style cleanup

--- dataview/transports/json_rpc.py
+++ dataview/transports/json_rpc.py
@@ -1,18 +1,18 @@
 import requests
 import json
+
 from dataview.models import X509Certificate
 from django.core.exceptions import ObjectDoesNotExist
 
-class JSONRPCClient:
+
+class JSONRPCClient(object):
     def __init__(self):
         self.request_id = 1
-        pass
 
     def connect(self, target, apikey, certificate=None):
         self.target = target
         self.apikey = apikey
         self.certificate = certificate
-        pass
 
     def disconnect(self):
         pass # this transport does not stay open
@@ -20,20 +20,23 @@ class JSONRPCClient:
     def call(self, command, arguments):
         req = {"jsonrpc": "2.0", "method": command, "params": arguments, "id": self.request_id}
         self.request_id +=1
+
         if self.certificate is not None:
             try:
                 cert_file = X509Certificate.get_file_from_str(self.certificate)
             except ObjectDoesNotExist:
                 cert_file = X509Certificate.create_from_str(self.certificate).file_name
+
             r = requests.post(self.target, data = json.dumps(req),
-                              headers = { 'Authorization': 'Token ' + self.apikey },
+                              headers={'Authorization': 'Token ' + self.apikey},
                               verify=cert_file)
         else:
             r = requests.post(self.target, data = json.dumps(req),
-                  headers = { 'Authorization': 'Token: ' + self.apikey })
+                              headers={'Authorization': 'Token: ' + self.apikey})
 
         if r.status_code == 200:
             return r.json()['result']
+
         raise Exception(r.status_code)
 
     def get_client(self):
