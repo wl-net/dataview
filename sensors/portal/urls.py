c71ab99dd80b35799f1067117b79be1485cd9ac7
Andrew Sorensen <andrew@andrewsorensen.net>
2016-09-10 20:10:26 -0700

    Sensor basics

--- sensors/portal/urls.py
+++ sensors/portal/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from sensors.views import index
+from sensors.views import index, edit, observe, data
 
-urlpatterns = patterns('automation.views',
+urlpatterns = [
     url(r'^/?$', index, name = 'sensor-index'),
-)
+    url(r'^/(?P<sensor>[0-9a-f\-]+)/edit$', edit, name = 'sensors-edit_sensor'),
+    url(r'^/(?P<sensor>[0-9a-f\-]+)/observe$', observe, name = 'sensors-observe_sensor'),
+    url(r'^/(?P<sensor>[0-9a-f\-]+)/data', data, name = 'sensors-data_sensor'),
+]
 
--- sensors/views.py
+++ sensors/views.py
@@ -1,11 +1,36 @@
-from django.template import RequestContext
-from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from django.shortcuts import render
 
 from guardian.shortcuts import get_objects_for_user
+
+import json
 
 
 def index(request):
     sensors = get_objects_for_user(request.user, 'sensors.change_sensor')
 
-    return render_to_response('sensors/index.html',
-                              RequestContext(request, {'sensors': sensors}))
+    return render(request, 'sensors/index.html', {'sensors': sensors})
+
+
+def edit(request, sensor):
+    sensor = get_objects_for_user(request.user, 'sensors.change_sensor').get(id=sensor)
+    return render(request, 'sensors/edit.html', {'sensor': sensor})
+
+
+def observe(request, sensor):
+    sensor = get_objects_for_user(request.user, 'sensors.change_sensor').get(id=sensor)
+    values = sensor.get_values()
+    return render(request, 'sensors/observe.html', {'sensor': sensor, 'values': values})
+
+
+def data(request, sensor):
+    sensor = get_objects_for_user(request.user, 'sensors.change_sensor').get(id=sensor)
+    values = sensor.get_values(200)
+
+    x = []
+    y = []
+    for value in values:
+        x.append(str(value.updated))
+        y.append(value.value)
+
+    return HttpResponse(json.dumps({'data':{'x': x, 'y': y}}), content_type='application/json')
