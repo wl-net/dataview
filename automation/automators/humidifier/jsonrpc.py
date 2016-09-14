a8f9d0eaec7b67b4da1ee4a3d567a93c7554c38b
Andrew Sorensen <andrew@andrewsorensen.net>
2016-09-13 18:42:24 -0700

    Commit package change, stub for healthcheck

--- automation/automators/humidifier/jsonrpc.py
+++ automation/automators/humidifier/jsonrpc.py
@@ -1,11 +1,23 @@
-from dataview.transports.json_rpc import JSONRPCClient
+from dataview.transports.jsonrpc import JSONRPCClient
 from automation.automators import AbstractAutomator
 
+
 class HumidiferJSONRPCAutomator(AbstractAutomator):
+
     def __init__(self, configuration):
         super().__init__(configuration)
         self.client = JSONRPCClient()
         self.client.connect(configuration['target'], configuration['token'], configuration['certificate'])
+
+    @staticmethod
+    def get_configuration_fields():
+        fields = {
+            'target': ['text', 'url'],
+            'token': ['text', 'secret'],
+            'certificate': ['text', 'pemfile'],
+        }
+
+        return fields
 
     def set_fan_speed(self, fan_speed):
         self.client.call('call_function', ['humfanspeed', [fan_speed.upper()]])
@@ -15,3 +27,6 @@ class HumidiferJSONRPCAutomator(AbstractAutomator):
 
     def get_commands(self):
         return {"set_fan_speed":{"args":[["off", "high", "low"]]}, "set_output_rate":{"args":[["off", "high", "low"]]}}
+
+    def healthcheck(self):
+        self.client.call({'healthcheck', []})
