1588fa93ef48e1ae741edadbc74b99109028e156
Andrew Sorensen <andrew@andrewsorensen.net>
2017-12-06 22:01:17 -0800

    Add client-side CSRF token passing support

--- dataview/static/js/dataview.js
+++ dataview/static/js/dataview.js
@@ -5,6 +5,34 @@ var config = {};
     $(document).ready(function() {
         console.debug("dataview.js ready()");
         $.fn.dataview.loadConfig()
+
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        var csrftoken = getCookie('csrftoken');
+        function csrfSafeMethod(method) {
+            // these HTTP methods do not require CSRF protection
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            }
+});
     });
 
     $.fn.dataview = {};
