8a40b8b506a5ed4cdf0d78bf95b797b25a3b8969
Andrew Sorensen <andrew@andrewsorensen.net>
2018-01-29 19:19:39 -0800

    Fix formatting

--- automation/sign_widgets/media/__init__.py
+++ automation/sign_widgets/media/__init__.py
@@ -4,20 +4,20 @@ from automation.models import Automator
 
 class MediaPlayerWidget(AbstractWidget):
 
-    WIDGET_NAME = "Media Player"
+  WIDGET_NAME = "Media Player"
 
-    def __init__(self, configuration):
-        super().__init__(configuration)
-        pass
+  def __init__(self, configuration):
+    super().__init__(configuration)
+    pass
 
-    def get_contents(self):
-        try:
-            res = Automator.objects.get(id=self.configuration['automator']).get_instance().get_playback_information()
-            return {'current_title': res['current']['song'], 'title': res['current']['title'],
-                    'volume': res['playback_information']['volume']['numeric'],
-                    'is_playing': res['playback_information']['is_playing'],
-                    'position': res['playback_information']['time']['numeric'],
-                    'length': res['playback_information']['length']['numeric']}
-        except Exception:
-            return "Unknown"
+  def get_contents(self):
+    try:
+      res = Automator.objects.get(id=self.configuration['automator']).get_instance().get_playback_information()
+      return {'current_title': res['current']['song'], 'title': res['current']['title'],
+              'volume': res['playback_information']['volume']['numeric'],
+              'is_playing': res['playback_information']['is_playing'],
+              'position': res['playback_information']['time']['numeric'],
+              'length': res['playback_information']['length']['numeric']}
+    except Exception:
+      return "Unknown"
 
